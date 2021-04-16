979b9dc32564624d459a6bc34f22808a61e63a46
Gabriel Lugmayr <g.lugmayr@htlkrems.at>
2021-04-16 09:48:24 +0200

    edited title

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
    return (
       <>
       <Head>
-        <title>Hetzi Liebe</title>
+        <title>Compliment</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Test />
